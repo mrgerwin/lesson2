3c530ea658e851682cf07bb503c212390822da04
mrgerwin <jgerwin@otsegoknights.org>
2018-07-19 09:34:22 -0400

    Create coffee.py

---
+++ coffee.py
@@ -0,0 +1,13 @@
+def printReceipt(name, coffeeNum, hChocolateNum, totalCost):
+  print("*************************************")
+  print("Thank you for purchasing from Knights Coffee Shop " + name)
+  print(" ")
+  print("Coffees..............." + str(coffeeNum)+" x 0.25")
+  print("Hot Chocolates........" + str(hChocolateNum) + " x 0.50")
+  print("----------------------------------")
+  print("Total Cost.............$"+str(totalCost))
+  name = "John Doe"
+  coffeeNum = 3
+  hChocolateNum = 2
+  totalCost = 1.75
+printReceipt(name, coffeeNum, hChocolateNum, totalCost)
